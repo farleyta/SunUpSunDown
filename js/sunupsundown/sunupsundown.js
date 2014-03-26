121d0c1bc91a2c3b0732422468874bdf014b41dd
Tim Farley <me@timfarley.com>
2014-03-26 18:38:40 -0400

    Added XHR URL prep function.

--- js/sunupsundown/sunupsundown.js
+++ js/sunupsundown/sunupsundown.js
@@ -24,7 +24,20 @@ var sunupsundown = (function () {
 
     // Build and return a URL for the AJAX request
     function buildXHR(lat,lng) {
-        return "http://www.earthtools.org/sun/" + lat + "/" + lng + "/4/12/-5/0";
+        var currentDate = new Date(),
+            day = currentDate.getDate(),
+            mon = currentDate.getMonth(),
+            utcOffset = createOffset(currentDate);
+
+        // Construct the Earthtools URL - note that utcOffset will differ 
+        // depending on DST, so we always set the final field to 0 (false)
+        return "http://www.earthtools.org/sun/" + lat + "/" + lng + "/" + day + "/" + mon + "/" + utcOffset + "/0";
+    }
+
+    // Create the UTC offset from a given JS timezoneOffset value
+    function createOffset(date) {
+        var sign = (date.getTimezoneOffset() > 0) ? "-" : "";
+        return sign + Math.floor( Math.abs(date.getTimezoneOffset()) / 60);
     }
     
     // first, use the Geolocation API to get the Lat/Lng
